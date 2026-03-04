7f5480d46f5ee8a6966cc8406e9cbf82cac6a80f
vignesh <vignesh33319@gmail.com>
2026-03-04 18:39:04 +0530

    added sample py file

---
+++ SampleFile.py
@@ -0,0 +1,14 @@
+print("Welcome to Python class")
+
+userName = "vignesh"
+exp = 10
+is_active = True
+value = 12.000
+print(userName)
+print(exp)
+print(is_active)
+
+print(type(userName))
+print(type(exp))
+print(type(is_active))
+print(type(value))
